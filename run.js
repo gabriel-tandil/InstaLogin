6f1c074597f2430ed4eba6a4af18fb64d9e30dfa
Gabriel Alvarez <gabriel.tandil@gmail.com>
2020-05-13 14:35:26 -0300

    patras unos cambios

--- run.js
+++ run.js
@@ -1,7 +1,6 @@
 require('console-stamp')(console);
 
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
+
 const instagramprivateapi = require("instagram-private-api");
 //import { 
 //  IgApiClient, 
@@ -16,8 +15,8 @@ const Bluebird = require( 'bluebird');
 
 const ig = new instagramprivateapi.IgApiClient()
 
-async( function login () {
-  const { username, password } = await (inquirer.prompt([
+async function login () {
+  const { username, password } = await inquirer.prompt([
     { 
       type: 'input', 
       name: 'username', 
@@ -29,17 +28,17 @@ async( function login () {
       message: 'Password',
       mask: true
     }
-  ]))
+  ])
   // Login
-  await (Bluebird.try(async () => {
+  await Bluebird.try(async () => {
     ig.state.generateDevice(username);
-    await (ig.simulate.preLoginFlow());
-    const account = await (ig.account.login(username, password));
+    await ig.simulate.preLoginFlow();
+    const account = await ig.account.login(username, password);
     console.log('login OK')
-  }))
+  })
   // Two-factor
   .catch(instagramprivateapi.IgLoginTwoFactorRequiredError, async (e) => {
-    const { twoFactorMethod, twoFactorCode } = await (inquirer.prompt([
+    const { twoFactorMethod, twoFactorCode } = await inquirer.prompt([
       { 
         type: 'list', 
         name: 'twoFactorMethod', 
@@ -54,33 +53,33 @@ async( function login () {
         name: 'twoFactorCode', 
         message: '2FA code' 
       }
-    ]))
-    await (ig.account.twoFactorLogin({
+    ])
+    await ig.account.twoFactorLogin({
       twoFactorIdentifier: e.response.body.two_factor_info.two_factor_identifier,
       verificationMethod: twoFactorMethod,
       verificationCode: twoFactorCode,
       username: username
-    }))
+    })
     console.log('two-factor login OK')
   })
   // Challenge
   .catch(instagramprivateapi.IgCheckpointError, async () => {
     console.log('checkpoint', ig.state.checkpoint); // Checkpoint info here
     console.log('challenge', ig.state.challenge)
-    await (ig.challenge.auto(true)); // Requesting sms-code or click "It was me" button
+    await ig.challenge.auto(true); // Requesting sms-code or click "It was me" button
     console.log('checkpoint', ig.state.checkpoint); // Challenge info here
     console.log('challenge', ig.state.challenge)
-    const { code } = await (inquirer.prompt([
+    const { code } = await inquirer.prompt([
       { type: 'input', name: 'code', message: 'Challenge code' }
-    ]))
-    console.log(await (ig.challenge.sendSecurityCode(code)));
+    ])
+    console.log(await ig.challenge.sendSecurityCode(code));
     console.log('challenge', ig.state.challenge)
   })
-})
+}
 
 // Main
 (async () => {
-  await (login ());
-  const followers = await (ig.feed.pendingFriendships().items())
+  await login ();
+  const followers = await ig.feed.pendingFriendships().items()
   console.log(followers)
 })()
